edbed08217ad57b443de75e5e3afc6cf31234ffc
krzm <kmazanek@gmail.com>
2022-10-08 12:36:35 +0200

    use separete files for classes

---
+++ Pong/Timer.js
@@ -0,0 +1,15 @@
+export class Timer {
+    timer() {
+        this.deltaTime = 0;
+        this.lastTime = 0;
+    }
+
+    set(timestamp) {
+        this.deltaTime = timestamp - this.lastTime;
+        this.lastTime = timestamp;
+    }
+
+    log() {
+        console.log(this.deltaTime);
+    }
+}
--- Pong/pongScript1.js
+++ Pong/pongScript1.js
@@ -1,31 +1,15 @@
+import {Timer} from './Timer.js';
+
 const canvas = document.getElementById('canvas1');
 const WIDTH = canvas.width = 600;
 const HEIGHT = canvas.height = 600;
 const ctx = canvas.getContext('2d');
 
-class timer {
-    timer() {
-        this.deltaTime = 0;
-        this.lastTime = 0;
-    }
-    
-    set(timestamp)
-    {
-        this.deltaTime = timestamp - this.lastTime;
-        this.lastTime = timestamp;
-    }
-
-    log()
-    {
-        console.log(this.deltaTime);
-    }
-}
-
-var frameTimer = new timer();
+var timer = new Timer();
 
 function animate(timestamp){
-    frameTimer.set(timestamp);
-    //frameTimer.log();
+    timer.set(timestamp);
+    //timer.log();
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.fillRect(0,50,100,100);
     requestAnimationFrame(animate);
