2e4d5e7d3709f6f4b5fd203b105a3cac26bde3c2
krzm <kmazanek@gmail.com>
2022-10-08 12:21:40 +0200

    add timer class

--- Pong/pongScript1.js
+++ Pong/pongScript1.js
@@ -2,13 +2,33 @@ const canvas = document.getElementById('canvas1');
 const WIDTH = canvas.width = 600;
 const HEIGHT = canvas.height = 600;
 const ctx = canvas.getContext('2d');
-let x = 0;
 
-function animate(){
+class timer {
+    timer() {
+        this.deltaTime = 0;
+        this.lastTime = 0;
+    }
+    
+    set(timestamp)
+    {
+        this.deltaTime = timestamp - this.lastTime;
+        this.lastTime = timestamp;
+    }
+
+    log()
+    {
+        console.log(this.deltaTime);
+    }
+}
+
+var frameTimer = new timer();
+
+function animate(timestamp){
+    frameTimer.set(timestamp);
+    //frameTimer.log();
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    ctx.fillRect(x,50,100,100);
-    x++;
+    ctx.fillRect(0,50,100,100);
     requestAnimationFrame(animate);
 };
 
-animate();
+animate(0);
