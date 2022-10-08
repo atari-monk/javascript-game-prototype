c453019265e704a6faaeedb7654b913bf7f57ee9
krzm <kmazanek@gmail.com>
2022-10-08 22:10:59 +0200

    Add private methods to player

--- Framework/Player.js
+++ Framework/Player.js
@@ -5,12 +5,29 @@ export class Player extends GameObject {
         super(game, position, size, speed);
     }
     update(timer, input) {
+        this.#handleInput(input);
+        this.#setPosition(timer);
+        this.#setConstraints();   
+    }
+    #handleInput(input) {
         if (input.keys.indexOf("ArrowUp") > -1) {
             this.speed.y = 1;
         } else if (input.keys.indexOf("ArrowDown") > -1) {
             this.speed.y = -1;
         } else this.speed.y = 0;
+    }
+    #setPosition(timer) {
         this.position.y -= timer.deltaTime * this.speed.y;
+    }
+    #setConstraints() {
+        if (this.position.y <= 0)
+        {
+            this.position.y = 0;
+        }
+        if (this.position.y >= this.game.height - this.size.y)
+        {
+            this.position.y = this.game.height - this.size.y;
+        }
     }
     draw(ctx) {
         ctx.fillRect(
