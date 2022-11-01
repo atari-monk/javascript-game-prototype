cf381d8b29dcfe4f050cff78836bdbcb8ac0a121
krzm <kmazanek@gmail.com>
2022-11-01 23:24:49 +0100

    refactor nav section

--- indexScript.js
+++ indexScript.js
@@ -1,48 +1,46 @@
 const pongPage = "Pong/pongPage.html";
-const samples = [];
-samples["direction"] = {
-    page: "Pong/Direction/direction.html",
-    script: "Direction.js",
-};
-samples["slider"] = {
-    page: "Pong/Slider/slider.html",
-    script: "Slider.js",
-};
-samples["wall-collision-test"] = {
+const tests = [];
+tests["wall-collision-test"] = {
     page: pongPage,
     script: "WallCollisionTest/WallCollisionTest.js",
 };
-samples["wall-collision"] = {
-    page: pongPage,
-    script: "WallCollision/WallCollision.js",
-};
-samples["player-test"] = {
+tests["player-test"] = {
     page: pongPage,
     script: "PlayerTest/PlayerTest.js",
 };
-samples["collision-left-test"] = {
+tests["collision-left-test"] = {
     page: pongPage,
     script: "CollisionLeftTest/CollisionLeftTest.js",
 };
-samples["collision-right-test"] = {
+tests["collision-right-test"] = {
     page: pongPage,
     script: "CollisionRightTest/CollisionRightTest.js",
 };
-samples["collision"] = {
+
+const prototypes = [];
+prototypes["direction"] = {
+    page: "Pong/Direction/direction.html",
+    script: "Direction.js",
+};
+prototypes["slider"] = {
+    page: "Pong/Slider/slider.html",
+    script: "Slider.js",
+};
+prototypes["wall-collision"] = {
+    page: pongPage,
+    script: "WallCollision/WallCollision.js",
+};
+prototypes["collision"] = {
     page: pongPage,
     script: "Collision/Collision.js",
 };
-samples["second-player"] = {
-    page: pongPage,
-    script: "SecondPlayer/SecondPlayer.js",
-};
 
-function update() {
-    var select = document.getElementById('select1');
+function selectTest(){
+    var select = document.getElementById('select-test');
     var option = select.options[select.selectedIndex];
     if (option.value == 'none')
         return;
-    var sample = samples[option.value];
+    var test = tests[option.value];
 
     const encodeGetParams = (p) =>
         Object.entries(p)
@@ -51,13 +49,34 @@ function update() {
 
     const params = {
         none: '',
-        script: sample.script,
+        script: test.script,
         title: option.text
     };
 
-    const url = sample.page + '?' + encodeGetParams(params);
+    const url = test.page + '?' + encodeGetParams(params);
 
     window.open(url, '_blank');
 }
 
-update();
+function selectPrototype(){
+    var select = document.getElementById('select-prototype');
+    var option = select.options[select.selectedIndex];
+    if (option.value == 'none')
+        return;
+    var prototype = prototypes[option.value];
+
+    const encodeGetParams = (p) =>
+        Object.entries(p)
+            .map((kv) => kv.map(encodeURIComponent).join("="))
+            .join("&");
+
+    const params = {
+        none: '',
+        script: prototype.script,
+        title: option.text
+    };
+
+    const url = prototype.page + '?' + encodeGetParams(params);
+
+    window.open(url, '_blank');
+}
