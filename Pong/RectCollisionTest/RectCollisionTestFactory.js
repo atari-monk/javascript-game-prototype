2647cb4ff3ceb16e40f59ffa43b56124d4a50e3e
krzm <kmazanek@gmail.com>
2022-11-09 22:57:51 +0100

    Add balnk collision

--- Pong/RectCollisionTest/RectCollisionTestFactory.js
+++ Pong/RectCollisionTest/RectCollisionTestFactory.js
@@ -4,15 +4,12 @@ import { InputHandlerEmpty } from "../../Framework/InputHandlerEmpty.js";
 import { InputHandler } from "../../Framework/InputHandler.js";
 import { Ball } from "../../Framework/Ball/Ball.js";
 import { BallPrinter } from "../../Framework/Ball/BallPrinter.js";
-import { BallInputHandler } from "../../Framework/Ball/BallInputHandler.js";
 import { BallInputHandler2 } from "../../Framework/Ball/BallInputHandler2.js";
 import { BallRender2 } from "../../Framework/Ball/BallRender2.js";
 import { WallCollision } from "../../Framework/WallCollision/WallCollision.js";
 import { Player } from "../../Framework/Player/Player.js";
 import { PlayerPrinter } from "../../Framework/Player/PlayerPrinter.js";
-import { BallCollisionLeft } from "../../Framework/BallCollision/BallCollisionLeft.js";
-import { BallCollisionPrinter } from "../../Framework/BallCollision/BallCollisionPrinter.js";
-import { BallCollisionRender } from "../../Framework/BallCollision/BallCollisionRender.js";
+import { RectCollision } from "../../Framework/BallCollision/RectCollision.js";
 import { PlayerRender } from "../../Framework/Player/PlayerRender.js";
 
 export class RectCollisionTestFactory {
@@ -38,18 +35,9 @@ export class RectCollisionTestFactory {
             , new InputHandlerEmpty()
             , new PlayerRender()
         );
-        this.collisionLeft = new BallCollisionLeft();
-
+        this.collisionLeft = new RectCollision();
         this.ballPrinter = new BallPrinter(this.ctx, this.ball);
         this.player1Printer = new PlayerPrinter(this.ctx
             , this.player1);
-        this.collisionLeftPrinter = 
-            new BallCollisionPrinter(
-                this.ctx
-                , this.collisionLeft);
-        this.collisionLeftRender = 
-            new BallCollisionRender(
-                this.ctx
-                , this.collisionLeft);
     }
 }
