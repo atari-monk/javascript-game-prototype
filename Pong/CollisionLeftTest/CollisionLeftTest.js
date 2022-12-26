800733af4e44a1f94206bb3bdf3731fcb192e153
atari-monk <atari.monk1@gmail.com>
2022-12-26 16:08:34 +0100

    fix leftt collison

--- Pong/CollisionLeftTest/CollisionLeftTest.js
+++ Pong/CollisionLeftTest/CollisionLeftTest.js
@@ -1,8 +1,6 @@
 import { Game } from '../../Framework/Game.js';
-import { GameInfo } from '../../Framework/GameInfo.js';
 import { Vector2 } from '../../Framework/Vector2.js';
 import { CollisionLeftTestFactory } from './CollisionLeftTestFactory.js';
-import { CollisionLeftTestInfoFactory } from './CollisionLeftTestInfoFactory.js';
 
 const gameCanvas = document.getElementById('gameCanvas');
 gameCanvas.width = 680;
@@ -14,25 +12,26 @@ infoCanvas.width = 400;
 infoCanvas.height = 200;
 const infoCtx = infoCanvas.getContext('2d');
 
-var gameFactory = new CollisionLeftTestFactory(gameCtx
-    , new Vector2(gameCanvas.width, gameCanvas.height));
-var game = new Game(gameCtx
-    , new Vector2(gameCanvas.width, gameCanvas.height)
-    , gameFactory);
+var gameFactory = new CollisionLeftTestFactory(
+  gameCtx
+  , new Vector2(gameCanvas.width, gameCanvas.height)
+  , infoCtx
+  , new Vector2(infoCanvas.width, infoCanvas.height));
+var game = new Game(gameFactory);
 
-var infoFactory = new CollisionLeftTestInfoFactory(infoCtx
-  , new Vector2(infoCanvas.width, infoCanvas.height)
-  , gameFactory);
-var info = new GameInfo(infoCtx
-  , new Vector2(infoCanvas.width, infoCanvas.height)
-  , infoFactory);
+// var infoFactory = new CollisionLeftTestInfoFactory(infoCtx
+//   , new Vector2(infoCanvas.width, infoCanvas.height)
+//   , gameFactory);
+// var info = new GameInfo(infoCtx
+//   , new Vector2(infoCanvas.width, infoCanvas.height)
+//   , infoFactory);
 
 function animate(timestamp) {
-    game.update(timestamp);
-    info.update();
-    game.draw();
-    info.draw();
-    requestAnimationFrame(animate);
+  game.update(timestamp);
+  //info.update();
+  game.draw();
+  //info.draw();
+  requestAnimationFrame(animate);
 };
 
 animate(0);
--- Pong/CollisionLeftTest/CollisionLeftTestFactory.js
+++ Pong/CollisionLeftTest/CollisionLeftTestFactory.js
@@ -3,41 +3,49 @@ import { Vector2 } from "../../Framework/Vector2.js";
 import { InputHandlerEmpty } from "../../Framework/InputHandlerEmpty.js";
 import { InputHandler } from "../../Framework/InputHandler.js";
 import { Ball } from "../../Framework/Ball/Ball.js";
-import { BallInputHandler2 } from "../../Framework/Ball/BallInputHandler2.js";
+import { BallInputHandler } from "../../Framework/Ball/BallInputHandler.js";
 import { BallRender2 } from "../../Framework/Ball/BallRender2.js";
 import { WallCollision } from "../../Framework/WallCollision/WallCollision.js";
 import { Player } from "../../Framework/Player/Player.js";
 import { BallCollisionLeft } from "../../Framework/BallCollision/BallCollisionLeft.js";
 import { BallCollisionRender } from "../../Framework/BallCollision/BallCollisionRender.js";
 import { PlayerRender } from "../../Framework/Player/PlayerRender.js";
+import { Printer } from "../../Framework/Printer.js";
 
 export class CollisionLeftTestFactory {
-  constructor(ctx, size) {
-    this.ctx = ctx;
-    this.size = size;
+  constructor(gameCtx, gameSize, textCtx, textSize) {
+    this.gameCtx = gameCtx;
+    this.gameSize = gameSize;
+    this.textCtx = textCtx;
+    this.textSize = textSize;
     this.timer = new Timer();
     this.input = new InputHandler();
+    this.printer = new Printer(this.textCtx);
+
     this.ball = new Ball(
       this,
-      new Vector2(this.size.x / 2 + 70, this.size.y / 2),
+      new Vector2(this.gameSize.x / 2 + 70, this.gameSize.y / 2),
       new Vector2(40, 40),
       new Vector2(0, 0),
-      new BallInputHandler2(this.input)
+      new BallInputHandler(this.input)
       , new WallCollision()
       , new BallRender2()
+      , this.printer
     );
+
     this.player1 = new Player(
       this,
-      new Vector2(this.size.x / 2, this.size.y / 2),
+      new Vector2(this.gameSize.x / 2, this.gameSize.y / 2),
       new Vector2(40, 200),
       new Vector2(0, 0)
       , new InputHandlerEmpty()
       , new PlayerRender()
+      , this.printer
     );
     this.collisionLeft = new BallCollisionLeft();
     this.collisionLeftRender =
       new BallCollisionRender(
-        this.ctx
+        this.gameCtx
         , this.collisionLeft);
   }
 }
