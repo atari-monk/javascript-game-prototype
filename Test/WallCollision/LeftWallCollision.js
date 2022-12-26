677ef1313ba76000cac887d202530485d9b625fd
atari-monk <atari.monk1@gmail.com>
2022-12-26 22:10:24 +0100

    resize

--- Test/WallCollision/LeftWallCollision.js
+++ Test/WallCollision/LeftWallCollision.js
@@ -2,7 +2,7 @@ import { Game } from '../../Framework/Game.js';
 import { LeftWallCollisionFactory } from './LeftWallCollisionFactory.js';
 import { ScreenSize } from '../../Framework/ScreenSize.js';
 
-const screen = new ScreenSize(400, 400, 1430, 700);
+const screen = new ScreenSize(600, 600, 800, 1000);
 var gameFactory = new LeftWallCollisionFactory(screen);
 var game = new Game(gameFactory);
 
