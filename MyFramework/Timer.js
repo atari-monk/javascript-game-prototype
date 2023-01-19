7730b72a7af5b95d0300ebabf9a83f02ed671019
atari-monk <atari.monk1@gmail.com>
2023-01-19 22:16:26 +0100

    Chain lest and right and loop it

--- MyFramework/Timer.js
+++ MyFramework/Timer.js
@@ -1,15 +1,17 @@
 export class Timer {
-    constructor() {
-        this.deltaTime = 0;
-        this.lastTime = 0;
-    }
+  constructor() {
+    this.deltaTime = 0;
+    this.lastTime = 0;
+  }
 
-    set(timestamp) {
-        this.deltaTime = timestamp - this.lastTime;
-        this.lastTime = timestamp;
-    }
+  set(timestamp) {
+    const delta = timestamp - this.lastTime;
+    if(delta < 20)
+      this.deltaTime = delta;
+    this.lastTime = timestamp;
+  }
 
-    log() {
-        console.log(this.deltaTime);
-    }
+  log() {
+    console.log(this.deltaTime);
+  }
 }
