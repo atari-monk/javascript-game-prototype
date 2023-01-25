c383be5ff90246f5a790cbf1b6b213a3c59d7943
atari-monk <atari.monk1@gmail.com>
2023-01-25 13:30:33 +0100

    remove laptop version part 2

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,45 +1,53 @@
 'use strict';
 
-import { Game } from '../../MyFramework/Game.js';
+import { Game2 } from '../../MyFramework/Game2.js';
 import { PongFactory } from './pongFactory.js';
-import { ScreenSize } from '../../MyFramework/ScreenSize.js';
+import { MultiCanvasData } from '../../MyFramework/MultiCanvasData.js';
+import { Menu } from './menu.js';
 
-var me = document.querySelector('script[data-name="myDynScript"]');
-const ver = me.getAttribute('ver');
+const box = document.querySelector(".box");
+const pageX = document.getElementById("x");
+const pageY = document.getElementById("y");
+const btnOverlay = document.getElementById("btn-overlay");
+const overlay = document.getElementById("overlay");
 
-if (ver === "laptop") {
-  const box = document.querySelector(".box");
-  const pageX = document.getElementById("x");
-  const pageY = document.getElementById("y");
-  const btn1 = document.getElementById("btn1");
-  const overlay = document.getElementById("overlay");
-
-  function updateDisplay(event) {
-    pageX.innerText = event.pageX;
-    pageY.innerText = event.pageY;
-  }
-
-  box.addEventListener("mousemove", updateDisplay, false);
-  box.addEventListener("mouseenter", updateDisplay, false);
-  box.addEventListener("mouseleave", updateDisplay, false);
-  btn1.addEventListener("click", on, false);
-  overlay.addEventListener("click", off, false);
-
-  function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+function updateDisplay(event) {
+  pageX.innerText = event.pageX;
+  pageY.innerText = event.pageY;
 }
 
-const screen = ver === 'laptop' ? new ScreenSize(800, 600, 650, 700) : new ScreenSize(300, 300, 500, 500);
+box.addEventListener("mousemove", updateDisplay, false);
+box.addEventListener("mouseenter", updateDisplay, false);
+box.addEventListener("mouseleave", updateDisplay, false);
+btnOverlay.addEventListener("click", on, false);
+overlay.addEventListener("click", off, false);
 
-var gameFactory = new PongFactory(screen);
-var game = new Game(gameFactory);
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
 
-game.ball.start();
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+const menu = new Menu();
+menu.setGameView();
+const game = new Game2(new PongFactory(new MultiCanvasData()));
+
+const btnStart = document.getElementById('btn-start');
+btnStart.addEventListener("click", startGame, false);
+const btnReset = document.getElementById('btn-reset');
+btnReset.addEventListener("click", resetGame, false);
+
+function startGame() {
+  game.ball.start();
+}
+function resetGame() {
+  game.player1.reset();
+  game.player2.reset();
+  game.ball.reset();
+  game.pointCount.reset();
+}
 
 function animate(timestamp) {
   game.update(timestamp);
--- Pong/pongFactory.js
+++ Pong/pongFactory.js
@@ -8,69 +8,73 @@ import { FloorCollision } from "../../MyFramework/WallCollision/FloorCollision.j
 import { Player } from "../../MyFramework/Player/Player.js";
 import { PlayerKeysInput } from "../../MyFramework/Player/PlayerKeysInput.js";
 import { RectCollision } from "../../MyFramework/BallCollision/RectCollision.js";
-import { PointCount } from "../../MyFramework/Ball/PointCount.js";
+import { PointCount2 } from "../../MyFramework/Ball/PointCount2.js";
 import { PlayerRender } from "../../MyFramework/Player/PlayerRender.js";
 import { Player2Render } from "../../MyFramework/Player/Player2Render.js";
 import { Printer } from "../../MyFramework/Printer.js";
 import { Bearing } from "../../MyFramework/Entity/Bearing.js";
 import { Renderer } from "../../MyFramework/Entity/Renderer.js";
 import { InfoRender } from "../../MyFramework/Entity/InfoRender.js";
+import { PlayerMouseInput } from "../../MyFramework/Player/PlayerMouseInput.js";
+import { MouseInputHandler } from "../../MyFramework/MouseInputHandler.js";
 
 export class PongFactory {
-  constructor(screen) {
-    this.screen = screen;
+  constructor(views) {
+    this.views = views;
+    this.infoCanvas = views.get('info');
+    this.gameCanvas = views.get('game');
     this.timer = new Timer();
-    this.printer = new Printer(this.screen.textCtx);
-    console.log(this.screen.textCtx);
+    this.printer = new Printer(this.infoCanvas.ctx);
     this.input = new KeysInputHandler();
+    this.mouseInput = new MouseInputHandler(this.printer);
     this.bearing = new Bearing();
 
     this.renderers = {
       'graphics': new Renderer(
-        [new BallRender() //, new DirectionRender()
+        [new BallRender()//, new DirectionRender()
         ]),
       'text': new Renderer([new InfoRender()])
     };
 
     this.ball = new BallV4(
-      this.screen.gameSize,
-      new Vector2(this.screen.gameSize.x / 2, this.screen.gameSize.y / 2),
+      this.gameCanvas.size,
+      new Vector2(this.gameCanvas.size.x / 2, this.gameCanvas.size.y / 2),
       new Vector2(10, 10),
       new Vector2(0, 0),
-      new InputHandlerEmpty(),
-      this.renderers,
-      this.printer,
-      new Vector2(0, 180),
-      new FloorCollision(),
-      this.bearing
+      new InputHandlerEmpty()
+      , this.renderers
+      , this.printer
+      , new Vector2(5, 5)
+      , new FloorCollision()
+      , this.bearing
     );
 
     this.player1 = new Player(
-      this.screen.gameSize,
-      new Vector2(10, this.screen.gameSize.y / 2),
+      this.gameCanvas.size,
+      new Vector2(10, this.gameCanvas.size.y / 2),
       new Vector2(10, 100),
-      new Vector2(0, 0),
-      new PlayerKeysInput(this.input),
-      new PlayerRender(),
-      this.printer,
-      new Vector2(0, 270),
-      'Green'
+      new Vector2(0, 0)
+      , new PlayerKeysInput(this.input)
+      , new PlayerRender()
+      , this.printer
+      , new Vector2(5, 30)
+      , 'Green'
     );
 
     this.player2 = new Player(
-      this.screen.gameSize,
-      new Vector2(this.screen.gameSize.x - 10, this.screen.gameSize.y / 2),
+      this.gameCanvas.size,
+      new Vector2(this.gameCanvas.size.x - 10, this.gameCanvas.size.y / 2),
       new Vector2(10, 100),
-      new Vector2(0, 0),
-      new PlayerKeysInput(this.input),
-      new Player2Render(),
-      this.printer,
-      new Vector2(0, 360),
-      'Blue'
+      new Vector2(0, 0)
+      , new PlayerMouseInput(this.mouseInput)
+      , new Player2Render()
+      , this.printer
+      , new Vector2(5, 55)
+      , 'Blue'
     );
 
     this.collisionLeft = new RectCollision();
     this.collisionRight = new RectCollision();
-    this.pointCount = new PointCount(this.screen, this.ball);
+    this.pointCount = new PointCount2(this.views, this.ball);
   }
 }
